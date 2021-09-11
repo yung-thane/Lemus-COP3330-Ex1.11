9f9e8c1d9e4daebd6f0bc043c48e6f12ffa74f0a
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-11 18:58:29 -0400

    Final Commit

--- src/Currency_Conversion.java
+++ src/Currency_Conversion.java
@@ -1,11 +1,24 @@
 /*
- *  UCF COP3330 Fall 2021 Assignment 1.10 Solution
+ *  UCF COP3330 Fall 2021 Assignment 1.11 Solution
  *  Copyright 2021 Ronald Lemus
  */
 
 
+import java.util.Scanner;
+
 public class Currency_Conversion {
     public static void main(String[] arg){
-        
+        System.out.print("How many euros are you exchanging? ");
+        Scanner euroInp = new Scanner(System.in);
+        int euros = euroInp.nextInt();
+
+        System.out.print("What is the exchange rate? ");
+        Scanner rateInp = new Scanner(System.in);
+        double rate = rateInp.nextDouble();
+
+        double dollars = euros * rate;
+
+        System.out.printf("%d euros at an exchange rate of %.4f is\n%.2f U.S. dollars.",euros, rate,dollars);
+
     }
 }
